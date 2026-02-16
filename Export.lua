5617eb7cd06e007787adafcf6e528decafe0579b
thespielplatz <informatics@gmx.net>
2026-02-16 20:28:54 +0100

    update: add parsing of usedAbilities

--- Export.lua
+++ Export.lua
@@ -1,5 +1,11 @@
 local PBR = PetBattleRecorder
 
+
+function PBR:InitExport()
+    self.usedAbilities = {}
+end
+
+
 ----------------------------------
 -- GET LOADOUT
 ----------------------------------
@@ -7,25 +13,33 @@ local PBR = PetBattleRecorder
 function PBR:GetLoadout()
     local text = ""
     for i = 1,3 do
-        local petGUID, ability1, ability2, ability3 =
-            C_PetJournal.GetPetLoadOutInfo(i)
+        local petGUID, ability1, ability2, ability3 = C_PetJournal.GetPetLoadOutInfo(i)
         if petGUID then
             local speciesID, customName, level, xp, maxXp,
             displayID, isFavorite, name, icon, petType,
-            creatureID =
-            C_PetJournal.GetPetInfoByPetID(petGUID)
-            text = text .. "[npc=" .. creatureID .. "] ("
+            creatureID, sourceText, description, isWild,
+            canBattle, tradable, unique, obtainable = C_PetJournal.GetPetInfoByPetID(petGUID)
+
+            text = text .. "[npc=" .. creatureID .. "]"
+            
+            text = " ("
             local abilities = {ability1, ability2, ability3}
             local idTable = C_PetJournal.GetPetAbilityList(speciesID)
 
-            for a=1,3 do
+            for a = 1,3 do
                 local abilityID = abilities[a]
                 if abilityID then
-                    local abilityIndex =
-                        abilityID == idTable[a] and 1 or 2
-                    text = text .. abilityIndex
+                    local abilityInLoadout = abilityID == idTable[a] and 1 or 2
+                    text = text .. abilityInLoadout
                 end
                 if a < 3 then text = text .. "/" end
+
+                local id, name, icon, maxCooldown, unparsedDescription, 
+                numTurns, petType, noStrongWeakHints = C_PetBattles.GetAbilityInfoByID(abilityID)
+                table.insert(self.usedAbilities, {
+                    name = name,
+                    id = id
+                })
             end
             text = text .. ")"
             if i < 3 then text = text .. ", " end
@@ -35,10 +49,19 @@ function PBR:GetLoadout()
 end
 
 ----------------------------------
+-- Convert Combat Line
+----------------------------------
+
+function PBR:ConvertCombatLine(line)
+end
+
+
+----------------------------------
 -- EXPORT TEXT
 ----------------------------------
 
 function PBR:GenerateExport()
+    PBR:InitExport()
     local text = self:GetLoadout()
     text = text .. "\n\n[ol]\n"
     for i, entry in ipairs(self.battleLog) do
