d9de275d44a8530e56f1ba7d76c42534b7c0b428
thespielplatz <informatics@gmx.net>
2026-02-18 21:14:15 +0100

    test: add log02 and log03

--- spec/BattleLogParser_spec.lua
+++ spec/BattleLogParser_spec.lua
@@ -1,11 +1,11 @@
 local assert = require("luassert")
 local BattleLogParser = require("core.BattleLogParser")
 
-local log01filePath = 'spec/fixtures/log01.txt'
-
 describe("core.BattleLogParser", function()
   it("check log01", function()
     local parser = BattleLogParser:new()
+    local log01filePath = 'spec/fixtures/log01.txt'
+
     local file = io.open(log01filePath, "r")
     assert.is_not_nil(file)
     for line in file:lines() do
@@ -13,5 +13,31 @@ describe("core.BattleLogParser", function()
     end
 
     assert.are.equal(parser.round, '8')
+  end)
+
+  it("check log02", function()
+    local parser = BattleLogParser:new()
+    local log02filePath = 'spec/fixtures/log02.txt'
+
+    local file = io.open(log02filePath, "r")
+    assert.is_not_nil(file)
+    for line in file:lines() do
+      parser:parseLine(line)
+    end
+
+    assert.are.equal(parser.round, '6')
+  end)
+
+  it("check log03", function()
+    local parser = BattleLogParser:new()
+    local log02filePath = 'spec/fixtures/log03.txt'
+
+    local file = io.open(log02filePath, "r")
+    assert.is_not_nil(file)
+    for line in file:lines() do
+      parser:parseLine(line)
+    end
+
+    assert.are.equal(parser.round, '11')
   end)
 end)
