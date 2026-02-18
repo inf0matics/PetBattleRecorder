d9843e85ef5ab5065a4267ebafb56264bda34936
thespielplatz <informatics@gmx.net>
2026-02-18 20:43:56 +0100

    test: add log01 as fixture and test for round length

--- core/BattleLogParser.lua
+++ core/BattleLogParser.lua
@@ -1,5 +1,5 @@
 local BattleLogParser = {
-  rounds = 0
+  round = 0,
 }
 
 function BattleLogParser:new()
@@ -7,9 +7,28 @@ function BattleLogParser:new()
   setmetatable(obj, self)
   self.__index = self
 
-  obj.rounds = 0
+  obj.round = 0
 
   return obj
 end
+
+function BattleLogParser:parseLine(line)
+  if line:find("Round") then
+      local tmp, newRound = line:match("(%w+)(.+)")
+      newRound = newRound:sub(2)
+
+      if self.round ~= newRound then
+          if newRound ~= "1" then
+              self:GenerateRound()
+          end
+          self.round = newRound
+      end
+  end
+  -- Process Line
+end
+
+function BattleLogParser:GenerateRound()
+end
+
 
 return BattleLogParser
--- spec/BattleLogParser_spec.lua
+++ spec/BattleLogParser_spec.lua
@@ -1,9 +1,17 @@
 local assert = require("luassert")
 local BattleLogParser = require("core.BattleLogParser")
 
+local log01filePath = 'spec/fixtures/log01.txt'
+
 describe("core.BattleLogParser", function()
-  it("check rounds", function()
+  it("check log01", function()
     local parser = BattleLogParser:new()
-    assert.are.equal(parser.rounds, 0)
+    local file = io.open(log01filePath, "r")
+    assert.is_not_nil(file)
+    for line in file:lines() do
+      parser:parseLine(line)
+    end
+
+    assert.are.equal(parser.round, '8')
   end)
 end)
